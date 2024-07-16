4c0809c598dd08c2b97fae7435584dfecc1e70d6
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-07-16 10:35:59 +0000

    added skills and location  class to project

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from users.models import Profile
+
+# Create your views here.
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {
+        "profiles" : profiles
+    }
+    return render(request, "users/profiles.html", context)
+
+def userProfile(request):
+    return render(request, "users/user-profile.html")
